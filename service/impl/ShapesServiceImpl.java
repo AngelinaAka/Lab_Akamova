b7320b95422f8e05a2a045c387d3f7a27895393d
AngelinaAka <style.2103@mail.ru>
2023-09-18 21:54:17 +0300

    Lab_r_Akamova

---
+++ service/impl/ShapesServiceImpl.java
@@ -0,0 +1,44 @@
+package service.impl;
+
+import colors.Color;
+import service.ShapesService;
+import shapes.Shape;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ShapesServiceImpl implements ShapesService {
+    @Override
+    public double getSquares(List<Shape> shapeList) {
+        double sum = 0;
+
+        for (int i = 0; i < shapeList.size(); i++) {
+            sum += shapeList.get(i).getS();
+        }
+
+        return sum;
+    }
+
+    @Override
+    public double getMaxPerimeters(List<Shape> shapeList) {
+        double maxP=0;
+
+        for (int i = 0; i < shapeList.size(); i++) {
+            if(maxP < shapeList.get(i).getP()){
+                maxP = shapeList.get(i).getP();
+            }
+        }
+
+        return maxP;
+    }
+
+    @Override
+    public Set<Color> getColor(List<Shape> shapeList) {
+        HashSet<Color> colors = new HashSet<Color>();
+        for (int i = 0; i < shapeList.size(); i++) {
+            colors.add(shapeList.get(i).getColor());
+        }
+        return colors;
+    }
+}
